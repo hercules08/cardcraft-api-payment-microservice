80942f1baa25495acabf67df6f498137fa39361f
Damola Omotosho <damola.omotosho@gmail.com>
2019-03-13 17:30:52 -0700

    Support static files

--- Cardcraft.Microservice.Payment/Startup.cs
+++ Cardcraft.Microservice.Payment/Startup.cs
@@ -1,21 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Threading.Tasks;
-using Cardcraft.Microservice.Account.BusinessLogic.Interface;
+﻿using Cardcraft.Microservice.Account.BusinessLogic.Interface;
 using Cardcraft.Microservice.Payment.BusinessLogic;
 using Cardcraft.Microservice.Payment.Clients;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 using Swashbuckle.AspNetCore.Swagger;
+using System;
+using System.IO;
+using System.Reflection;
 
 namespace Cardcraft.Microservice.Payment
 {
@@ -78,6 +72,7 @@ namespace Cardcraft.Microservice.Payment
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
             });
             //app.UseHttpsRedirection();
+            app.UseStaticFiles();
             app.UseMvc();
         }
     }
