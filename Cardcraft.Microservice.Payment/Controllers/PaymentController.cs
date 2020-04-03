3fcce08cbf12b7eeb07f2fe32c1657f22a7a5532
Damola Omotosho <damola.omotosho@gmail.com>
2020-04-02 21:36:47 -0700

    Add test payment endpoint

--- Cardcraft.Microservice.Payment/Controllers/PaymentController.cs
+++ Cardcraft.Microservice.Payment/Controllers/PaymentController.cs
@@ -50,6 +50,62 @@ namespace Cardcraft.Microservice.Payment.Controllers
         /// <param name="request"></param>
         /// <returns></returns>
         [HttpPost]
+        [Route("PurchaseCreditsTest")]
+        [ValidateModelState]
+        public async Task<IActionResult> PurchaseCreditsTest([FromBody]PurchaseCreditRequest request)
+        {
+            BillingInfo billInfo =
+                BillingsStub.BillingAmounts.Find(x => x.Id == request.BillingAmountId);
+
+            var charge = new ChargeCreateOptions
+            {
+                Amount = Convert.ToInt32(billInfo.Cost * 100),
+                Currency = "usd",
+                Description = "Cardcraft Purchase " + CONTEXT_USER,
+                SourceId = request.Token
+            };
+
+            var service = new ChargeService("sk_test_yqhJJAmFkjInHP9tJL4gR6G3");
+
+            Charge chargeResponse = null;
+
+            try
+            {
+                chargeResponse = service.Create(charge);
+
+                IAPIResponse updateUserCreditResponse = await _accountClient.UpdateUserCredits(new UpdateUserCreditRequest()
+                {
+                    UserProfileId = CONTEXT_USER,
+                    AccessToken = CONTEXT_TOKEN,
+                    NumOfCreditsToAdd = billInfo.CreditCount
+                });
+
+                if (updateUserCreditResponse.Success)
+                {
+                    APIResponse<UpdateUserCreditResponse> creditResponse = (APIResponse<UpdateUserCreditResponse>)updateUserCreditResponse;
+                    PurchaseCreditResponse purchaseCreditResponse = new PurchaseCreditResponse(true, "successful_purchase"
+                        , "Successfully added credits. Here are the total credits.", creditResponse.Data);
+
+                    return Ok(purchaseCreditResponse);
+                }
+            }
+            catch (StripeException ex)
+            {
+                StripeError stripeError = ex.StripeError;
+
+                // Handle error
+                return BadRequest(stripeError);
+            }
+
+            return BadRequest(new APIResponse(false, "unsuccessful_credit_add", "We were unable to add credits at the moment"));
+        }
+
+        /// <summary>
+        /// Allows user to purchase credit
+        /// </summary>
+        /// <param name="request"></param>
+        /// <returns></returns>
+        [HttpPost]
         [Route("PurchaseCredits")]
         [ValidateModelState]
         public async Task<IActionResult> PurchaseCredits([FromBody]PurchaseCreditRequest request)
